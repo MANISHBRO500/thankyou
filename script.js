c1256c4287c336e236f17035cc4b8c612f267583
MANISHBRO500 <160089365+MANISHBRO500@users.noreply.github.com>
2026-01-18 10:21:42 +0530

    Replace text in typeText function with new message
    Updated the text in the typeText function to include a longer message expressing gratitude and reflections on teaching.

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
-const text = "Thank you for guiding me, believing in me, and shaping my future.";
+const text = "Thank you so much for your years of teaching. I knew very few things earlier, but you were the one who started teaching us something new and meaningful. I have so many memories to share.
+
+One thing I truly want to mention is that the teaching strategies you used were wonderful and very honest. I personally noticed that because of your strategies, I was able to achieve a good score. You can check my previous records—especially in SST, I was an average student before.
+
+But you taught me how to study, and more importantly, how to understand what I study. Another lesson that really stayed with me is that it does not matter whether you start early or finish late; what truly matters is doing the work with perfection.
+
+These lessons will always stay with me, and I am genuinely grateful to you for shaping my learning and mindset";
 let index = 0;
 const speed = 80;
 
@@ -11,3 +17,4 @@ function typeText() {
 }
 
 window.onload = typeText;
+
